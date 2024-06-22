8cc066ea4c9176467494906dfd38728a0445ca88
Armaan <armaan33000@gmail.com>
2024-06-22 09:06:11 +0530

    a bit of landing page done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,21 +5,41 @@ const Navbar = () => {
     <nav className="p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-bold">
-          <a>ThePlotPPT</a>
+          <img
+            src="/logo.png"
+            className="mix-blend-multiply cursor-pointer"
+            style={{ width: "140px" }}
+          />
         </div>
 
-        <div className="flex space-x-4">
-          <a className="hover:text-gray-400">Home</a>
+        <div className="flex space-x-4 ml-32">
+          <a
+            className="hover:text-gray-700 cursor-pointer font-medium trasnition-all"
+            style={{ fontFamily: "Poppins" }}
+          >
+            Home
+          </a>
 
-          <a className="hover:text-gray-400">About Us</a>
+          <a
+            className="hover:text-gray-700 cursor-pointer font-medium transition-all"
+            style={{ fontFamily: "Poppins" }}
+          >
+            About Us
+          </a>
         </div>
 
         <div className="space-x-2">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Sign In
+          <button
+            className="bg-transparent border-2 border-gray-300 transition-all hover:bg-white text-black font-medium py-2 px-4 rounded"
+            style={{ fontFamily: "poppins" }}
+          >
+            Source code
           </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Sign Up
+          <button
+            className="bg-[#bdfe52] hover:bg-[#78a432] transition-all text-black font-semibold py-2 px-4 rounded-lg"
+            style={{ fontFamily: "Poppins" }}
+          >
+            Get Started
           </button>
         </div>
       </div>
--- src/landing/LandingPage.tsx
+++ src/landing/LandingPage.tsx
@@ -7,8 +7,51 @@ type Props = {};
 const LandingPage = (props: Props) => {
   return (
     <>
-    <Navbar/>
-      <div className="landingPageDiv">HI</div>
+      <Navbar />
+      <div
+        className="landingPageDiv flex flex-col justify-center items-center "
+        style={{
+          textAlign: "center",
+          justifyItems: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div className="version1Div w-full  " style={{ marginTop: "50px" }}>
+          <h4
+            style={{
+              fontFamily: "Jura, sans-serif",
+              background: "#e1e4e8",
+              padding: "6px",
+              paddingRight: "20px",
+              paddingLeft: "20px",
+              borderRadius: "20px",
+            }}
+          >
+            v1.0 - This Website from scratch
+          </h4>
+          <div className="superCoolTextDiv">
+            <h3>Get your</h3>
+            <h3>
+              pdfs in{" "}
+              <span
+                style={{
+                  border: "2px solid yellow",
+                  borderRadius: "30px",
+                  paddingLeft: "2px",
+                  paddingRight: "2px",
+                }}
+              >
+                seconds
+              </span>
+              <br />
+            </h3>
+            <h4>Revolutionize the way manage time</h4>
+            <h4>take the control back!</h4>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
